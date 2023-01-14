87822f1fefeebc858c3f4294fa75696bc6b639ee
mwoodpatrick <mwoodpatrick@gmail.com>
2023-01-14 12:20:56 -0800

    use port 8081 and ignore some more files

--- main.tf
+++ main.tf
@@ -1,25 +1,62 @@
+# Set the required provider and versions
 terraform {
   required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
+    # -> https://registry.terraform.io/providers/kreuzwerker/docker/latest
+    # https://medium.com/kreuzwerker-gmbh/maintaining-the-terraform-provider-for-docker-e7c90a0ef5f4
+    
     docker = {
-      source = "kreuzwerker/docker"
-      version = "~> 2.13.0"
+      source  = "kreuzwerker/docker"
+      version = "3.0.1"
     }
   }
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+# Configure the docker provider
+provider "docker" {
 }
 
+# Create a docker image resource
+# -> docker pull nginx:latest
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+# Create a docker container resource
+# -> same as 'docker run --name nginx -p8081:80 -d nginx:latest'
+# code-server is a web-based IDE that runs in a container and uses port 8080
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
+  name    = "nginx"
+  image   = docker_image.nginx.image_id
+
   ports {
+    external = 8081
     internal = 80
-    external = 8000
   }
 }
 
+# Or create a service resource
+# -> same as 'docker service create -d -p 8081:80 --name nginx-service --replicas 2 nginx:latest'
+resource "docker_service" "nginx_service" {
+  name = "nginx-service"
+  task_spec {
+    container_spec {
+      image = docker_image.nginx.repo_digest
+    }
+  }
+
+  mode {
+    replicated {
+      replicas = 2
+    }
+  }
+
+  endpoint_spec {
+    ports {
+      published_port = 8081
+      target_port    = 80
+    }
+  }
+}
